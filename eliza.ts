8eaa5d0cc897b61614bf5aaf787718fbd9a0b3d4
Joseph Atnafu <Joseph Atnafu>
2017-03-12 00:18:24 -0500

    added reassembly logic

--- eliza.ts
+++ eliza.ts
@@ -1,18 +1,24 @@
 class Eliza {
-    script: Object;
-    constructor(keywordData: Object[]) {
+    script: Map<string, KeywordData>;
+    constructor(keywordData: [string, number, string[]]) {
         this.buildScript(keywordData);
     }
-    buildScript(data): void {
+    buildScript(data: [string, number, string[]]): void {
         for (let x = 0; x < data.length; x++) {
             let k = new KeywordData(data[x][0], data[x][1], data[x][2]);
-            this.script[k.keyword] = k;
+            this.script.set(k.keyword, k);
         }
     }
     getResponse(input: string): string {
+        input = this.sanatize(input);
         let decompositionRules: string[][] = this.getDecompositionRules(input);
         let reassemblyRule: string = this.getReassemblyRule(input, decompositionRules);
+        if (reassemblyRule == null)
+            return this.genericResponse[this.randomNumIncl(0, this.genericResponse.length-1)];
         return this.reassemble(input, reassemblyRule);
+    }
+    sanatize(input: string): string {
+        return input.replace(/[\.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
     }
     getDecompositionRules(input: string): string[][] {
         let result: string[][];
@@ -26,9 +32,29 @@ class Eliza {
         return result;
     }
     getReassemblyRule(input: string, decompositionRules: string[][]): string {
-
+        let reassembRules: string[];
+        for (let arr of decompositionRules) {
+            let regex: RegExp = this.getRegExp(arr[0]);
+            if (regex.test(input)) {
+                return arr[this.randomNumIncl(1, arr.length-1)];
+            }
+        }
+        return null;
     }
-    reassemble(input: string, reassemblyRule: string): string {}
+    getRegExp(input: string): RegExp {
+        let transform: string = input.replace("*", ".+");
+        return new RegExp(transform);
+    }
+    randomNumIncl(min: number, max: number): number {
+        return (Math.floor(Math.random() * (max - min + 1)) + min);
+    }
+    reassemble(input: string, reassemblyRule: string): string {
+        let regex: RegExp = /\((0-9)\)/;
+        let words: string[] = input.split(/\s+/);
+        // replace all (n) in input with words[n], where n is a # between 0 and 9
+        let result: string = reassemblyRule.replace(regex, (match, p) => words[p[1]]);
+        return result;
+    }
 }
 
 class KeywordData {
@@ -43,7 +69,7 @@ class KeywordData {
 }
 
 /*
-keyword input = [[keyword, priority, [D, R...], ..., [D, R...]], ...]
+keyword input = [[keyword, priority, [[D, R...], ..., [D, R...]], ...]]
 transform it to:
 script = { KeywordData.keyword: KeywordData, ... }
 */
